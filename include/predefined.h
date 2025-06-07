cc896a3d6192c83b4df6923fac954bd94c332cac
metrovian <paramedoct@gmail.com>
2025-06-07 22:16:44 +0900

    기본 디렉터리 생성

---
+++ include/predefined.h
@@ -0,0 +1,14 @@
+// clang-format off
+#pragma once
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#define SYS_LOGGER_SYSTEM       "system"
+#define SYS_LOGGER_FORMAT       "log"
+#define SYS_LOGGER_SIZE         4096
+#define SYS_LOGGER_LOGLEVEL     2
+
+// clang-format on
