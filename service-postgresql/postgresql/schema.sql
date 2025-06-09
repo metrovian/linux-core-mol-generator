f84c714064327955515ce4b70fdb04b39cd9f361
metrovian <paramedoct@gmail.com>
2025-06-09 20:46:18 +0900

    데이터베이스 스키마 업데이트

--- service-postgresql/postgresql/schema.sql
+++ service-postgresql/postgresql/schema.sql
@@ -3,13 +3,13 @@ CREATE EXTENSION IF NOT EXISTS vector;
 CREATE TABLE molecule (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    smiles TEXT
+    smiles TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE mass_spectrum (
     id SERIAL PRIMARY KEY,
-    compound_id INTEGER REFERENCES molecule(id),
-    description TEXT,
+    molecule_id INTEGER REFERENCES molecule(id),
     spectrum_vector vector(1000),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
