d453a79ff449741145f295bea5c733c962e5287d
metrovian <paramedoct@gmail.com>
2025-06-15 00:18:01 +0900

    분자구조 검색 API 서버 생성

---
+++ source/server_api.c
@@ -0,0 +1,151 @@
+#include "server_api.h"
+#include "database_spectrum.h"
+#include "wrapper_openbabel.h"
+#include "wrapper_spdlog.h"
+#include "predefined.h"
+
+struct MHD_Daemon *server_api = NULL;
+struct MHD_Post {
+	char *url;
+	char *method;
+	char *data;
+	size_t size;
+};
+
+static char *server_api_request_mass(struct MHD_Post *post) {
+	char *name[EXTERNAL_NAME_MAX];
+	char *smiles[EXTERNAL_SMILES_MAX];
+	float peaks_data[SPECTRUM_MASS_BIN];
+	if (post->data) {
+		const char *post_data = post->data;
+		char *post_ptr = post->data;
+		for (int32_t i = 0; i < SPECTRUM_MASS_BIN; ++i) {
+			while (isspace((unsigned char)*post_data) || *post_data == ',') {
+				if (*post_data == '\0') {
+					log_error("failed to receive mass spectrum");
+					return NULL;
+				}
+
+				post_data++;
+			}
+
+			float peak_intensity = strtof(post_data, &post_ptr);
+			if (post_data == post_ptr) {
+				log_error("failed to parse mass spectrum");
+				return NULL;
+			}
+
+			peaks_data[i] = peak_intensity;
+			post_data = post_ptr;
+		}
+
+		if (database_spectrum_select_mass(name, smiles, peaks_data, EXTERNAL_NAME_MAX, EXTERNAL_SMILES_MAX, SPECTRUM_MASS_BIN) > 0) {
+			return mol_create((const char *)smiles);
+		}
+	}
+
+	log_critical("invalid mass spectrum");
+	return NULL;
+}
+
+static int32_t server_api_request_handler(
+    void *cls,
+    struct MHD_Connection *connection,
+    const char *url,
+    const char *method,
+    const char *version,
+    const char *data,
+    size_t *size,
+    void **con_cls) {
+	if (strncmp(method, "POST", 4) == 0) {
+		struct MHD_Post *con_post = *con_cls;
+		if (!con_post) {
+			con_post = calloc(1, sizeof(struct MHD_Post));
+			if (con_post) {
+				*con_cls = con_post;
+				return MHD_YES;
+			}
+
+			log_error("failed to allocate request body");
+			return MHD_NO;
+		}
+
+		if (*size > 0) {
+			char *data_realloc = realloc(con_post->data, con_post->size + *size + 1);
+			if (data_realloc) {
+				con_post->data = data_realloc;
+				memcpy(con_post->data + con_post->size, data, *size);
+				con_post->size += *size;
+				con_post->data[con_post->size] = 0;
+				*size = 0;
+				return MHD_YES;
+			}
+
+			free(con_post->data);
+			free(con_post);
+			log_error("failed to append request body");
+			return MHD_NO;
+		}
+
+		if (*size == 0) {
+			if (strncmp(url, "/mass", 5) == 0) {
+				char *mass_mol = server_api_request_mass(con_post);
+				if (!mass_mol) {
+					log_error("failed to create molecule from mass spectrum");
+					return MHD_NO;
+				} else {
+					struct MHD_Response *response =
+					    MHD_create_response_from_buffer(
+						strlen(mass_mol),
+						(void *)mass_mol,
+						MHD_RESPMEM_PERSISTENT);
+
+					int32_t ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+					MHD_destroy_response(response);
+					free(mass_mol);
+					return ret;
+				}
+			}
+		}
+	}
+
+	struct MHD_Response *response =
+	    MHD_create_response_from_buffer(
+		0,
+		NULL,
+		MHD_RESPMEM_PERSISTENT);
+
+	int32_t ret = MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
+	MHD_destroy_response(response);
+	return ret;
+}
+
+extern int8_t server_api_open() {
+	server_api =
+	    MHD_start_daemon(
+		MHD_USE_SELECT_INTERNALLY,
+		NET_API_PORT,
+		NULL,
+		NULL,
+		&server_api_request_handler,
+		NULL,
+		MHD_OPTION_END);
+
+	if (!server_api) {
+		log_error("failed to open api server");
+		return -1;
+	}
+
+	log_info("api service started");
+	return 0;
+}
+
+extern int8_t server_api_close() {
+	if (server_api) {
+		MHD_stop_daemon(server_api);
+		server_api = NULL;
+		log_info("api service terminated");
+	}
+
+	return 0;
+}
