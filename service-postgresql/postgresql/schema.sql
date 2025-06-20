60ca07e93eccf1de9d7824c6749cba4709338a96
metrovian <paramedoct@gmail.com>
2025-06-20 20:51:47 +0900

    데이터베이스 핵자기공명 스펙트럼 스키마 생성

--- service-postgresql/postgresql/schema.sql
+++ service-postgresql/postgresql/schema.sql
@@ -14,3 +14,10 @@ CREATE TABLE mass_spectrum (
     spectrum_vector vector(1000),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE nmr_spectrum (
+    id SERIAL PRIMARY KEY,
+    molecule_id INTEGER REFERENCES molecule(id),
+    spectrum_vector vector(1500),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
