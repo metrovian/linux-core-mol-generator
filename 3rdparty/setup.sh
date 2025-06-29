914e80b816498017588116b8cc86e803ce05906c
metrovian <paramedoct@gmail.com>
2025-06-29 14:53:43 +0900

    마이그레이션을 위한 서드파티 업데이트

--- 3rdparty/setup.sh
+++ 3rdparty/setup.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install chromium-browser openbabel
 
 sudo apt install \
+        libjansson-dev \
         libmicrohttpd-dev \
         libopenbabel-dev \
         libpq-dev \
